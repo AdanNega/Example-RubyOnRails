cec297697a0342199688375064a69985083fa8e0
Adan Negrete <adan.negrete2099@hotmail.com>
2022-11-04 11:45:06 -0600

    Create : Repository

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+        @article = Article.new
+        @article.title = 'Demo'
+    end
+
+    def create
+        @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+        render json: @article
+    end
+end
