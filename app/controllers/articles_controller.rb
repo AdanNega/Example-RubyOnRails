b0a8a9e3f3f7141f3d64089fef087b923ea5d8ef
Adan Negrete <adan.negrete2099@hotmail.com>
2022-11-09 09:50:33 -0600

    Second Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,20 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        render json: @article
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+        puts "\n\n\n #{@article.persisted?} \n\n\n"
+    end
     
     def new
         @article = Article.new
-        @article.title = 'Demo'
     end
 
     def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "articles/new", to: "articles#new"
   get "articles/:id", to: "articles#show"
   get "articles/:id/edit", to: "articles#edit"
+  
+  put "/articles/:id", to: "articles#update", as: :article
   post "articles", to: "articles#create"
 end
